015eca3dc94f0cc18ee3924fe8b8a4b14482834c
francisco.pacheco <167485560+franciscoimaginamos@users.noreply.github.com>
2024-10-15 16:11:43 -0500

    -constantes

---
+++ src/main/java/com/nissum/user/infrastructure/constants/Constants.java
@@ -0,0 +1,10 @@
+package com.nissum.user.infrastructure.constants;
+
+public class Constants {
+
+        public static final String SWAGGER_CREATE_SUMMARY = "CREAR NUEVO USUARIO";
+        public static final String SWAGGER_CREATE_DESCRIPTION = "SERVICIO PARA CREACION DE USUARIOS";
+
+
+
+}
